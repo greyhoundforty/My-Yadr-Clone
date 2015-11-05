b6b46f7c97a0cd7eb2b3b1c4d6ad74e834fa158a
greyhoundforty <rtiffany@my.lonestar.edu>
2015-11-05 13:52:49 -0600

    Corrected docker start

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -79,19 +79,13 @@ function ncz { netcat -z -v "$1" "$2"; }
 
 function search_notes { find $HOME/Dropbox/Ryans\ Docs/Work/CST_CSA\ Notes/ -iname "*.md" -print0| xargs -0 egrep "$@" }
 
-function dockip {
-  docker inspect --format '{{ .NetworkSettings.IPAddress }}' "$@"
-}
+function dockip { docker inspect --format '{{ .NetworkSettings.IPAddress }}' "$@" }
 
-function showfiles {
-defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app
-}
+function showfiles { defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app }
 
-function hidefiles {
-defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app
-}
+function hidefiles { defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app }
 
-function start_docker { bash -c "clear && DOCKER_HOST=tcp://192.168.99.100:2376 DOCKER_CERT_PATH=/Users/ryan/.docker/machine/machines/default DOCKER_TLS_VERIFY=1 /usr/local/bin/zsh" }
+function start_docker { docker-machine start default && eval "$(docker-machine env default)" }
 
 function local_hugo {
 docker stop local-hugo && docker rm local-hugo
@@ -100,7 +94,7 @@ hugo -D -t nofancy --baseUrl="http://tinybot.local:32773" --destination="/Users/
 docker run -d -P -p 32773:80 -v $HOME/docker_hugo:/usr/share/nginx/html --name local-hugo nginx
 }
 
-function remote_hugo {                                                                                                                            ⏎ master
+function remote_hugo {
 cd $HOME/Repos/hugobuild &&
 hugo -D -t nofancy --baseUrl="http://tinybot.io" --destination="/Users/ryan/Repos/greyhoundforty.github.io"
 cd $HOME/Repos/greyhoundforty.github.io && git add -A
